7acac66ed301d07378df654c2b10827d094b2551
Ineigo <m.guryanov@timeweb.ru>
2017-09-01 15:22:42 +0300

    Update Object:Canvas

--- Canvas.js
+++ Canvas.js
@@ -1,12 +1,12 @@
 export default class Canvas {
     static canvas = null;
     static context = null;
-    static create(canvas_tag_id) {
+    static create2d(canvas_tag_id = '') {
         const canvas = document.getElementById(canvas_tag_id);
         if(!canvas) throw new Error('Not element exists');
         Canvas.canvas = canvas;
         Canvas.context = canvas.getContext('2d');
-        console.info('Context created from', "#" + canvas_tag_id);
+        console.info('2d Context created from', "#" + canvas_tag_id);
         return Canvas.context;
     }
 };
