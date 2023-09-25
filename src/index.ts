3ffdbeef4a6363f4e1fcf31d42959200d07eef80
Guryanov Mikhail <m.guryanov@semrush.com>
2023-09-25 02:30:15 +0200

    Update to ts-script (1.0.1)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import Canvas from './objects/Canvas';
+import Layer from './objects/Layer';
+import Node from './objects/Node';
+import NodeImage from './objects/NodeImage';
+import NodeRect from './objects/NodeRect';
+import NodeText from './objects/NodeText';
+import Scene from './objects/Scene';
+import Vector2 from './objects/Vector2';
 import LinearMap from './LinearMap';
+import IKCore from './IKCore';
 
-export { Canvas, LinearMap };
+export { IKCore, LinearMap, Canvas, Layer, Node, NodeImage, NodeRect, NodeText, Scene, Vector2 };
