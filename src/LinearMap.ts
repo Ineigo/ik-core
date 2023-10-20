f0bfcfedfb96740896d55af0f1d62dc389bb99ba
Guryanov Mikhail <m.guryanov@semrush.com>
2023-10-20 21:45:51 +0200

    Apply Formating

--- src/LinearMap.ts
+++ src/LinearMap.ts
@@ -1,83 +1,83 @@
 export type LinearMapInitProps = {
-    width: number;
-    height: number;
-    boxSize: number;
-}
+  width: number;
+  height: number;
+  boxSize: number;
+};
 export default class LinearMap<T> {
-    private map: T[];
-    width: number;
-    height: number;
-    boxSize: number;
-    widthCount!: number;
-    heightCount!: number;
+  private map: T[];
+  width: number;
+  height: number;
+  boxSize: number;
+  widthCount!: number;
+  heightCount!: number;
 
-    constructor({ width, height, boxSize }: LinearMapInitProps) {
-        this.width = width;
-        this.height = height;
-        this.boxSize = boxSize;
-        this.map = new Array<T>(this.getMapItemCount());
+  constructor({ width, height, boxSize }: LinearMapInitProps) {
+    this.width = width;
+    this.height = height;
+    this.boxSize = boxSize;
+    this.map = new Array<T>(this.getMapItemCount());
+  }
+
+  fill = (func: () => T) => (typeof func == 'function' ? this.map.fill(func()) : this.map.fill(func));
+
+  each = (collback: (element: T) => T) => (this.map = this.map.map(collback));
+
+  shuffle() {
+    for (let i = this.map.length; i; i--) {
+      let j = Math.floor(Math.random() * i);
+      [this.map[i - 1], this.map[j]] = [this.map[j], this.map[i - 1]];
     }
+  }
 
-    fill = (func: () => T)  => (typeof func == 'function' ? this.map.fill(func()) : this.map.fill(func));
+  getIndex(x: number, y: number) {
+    const index = y * this.widthCount + x;
+    return index > this.map.length ? null : index;
+  }
 
-    each = (collback: (element: T) => T) => (this.map = this.map.map(collback));
-
-    shuffle() {
-        for (let i = this.map.length; i; i--) {
-            let j = Math.floor(Math.random() * i);
-            [this.map[i - 1], this.map[j]] = [this.map[j], this.map[i - 1]];
-        }
+  getItem = (x: number, y: number) => {
+    const index = this.getIndex(x, y);
+    if (index === null) {
+      return null;
     }
+    this.map[index];
+  };
 
-    getIndex(x: number, y: number) {
-        const index = y * this.widthCount + x;
-        return index > this.map.length ? null : index;
+  setItem(x: number, y: number, value: T) {
+    const index = this.getIndex(x, y);
+    if (index === null) {
+      return null;
     }
+    this.map[index] = value;
+  }
 
-    getItem = (x: number, y: number) => {
-        const index = this.getIndex(x, y);
-        if (index === null) {
-            return null;
-        }
-        this.map[index];
-    };
-
-    setItem(x: number, y: number, value: T) {
-        const index = this.getIndex(x, y);
-        if (index === null) {
-            return null;
-        }
-        this.map[index] = value;
-    }
-
-    [Symbol.iterator]() {
-        const $this = this;
-        let y = 0;
+  [Symbol.iterator]() {
+    const $this = this;
+    let y = 0;
+    return {
+      next() {
         return {
-            next() {
-                return {
-                    value: {
-                        [Symbol.iterator]() {
-                            let x = 0;
-                            return {
-                                next() {
-                                    return {
-                                        value: $this.getItem(x, y - 1),
-                                        done: x++ >= $this.widthCount,
-                                    };
-                                },
-                            };
-                        },
-                    },
-                    done: y++ >= $this.heightCount,
-                };
+          value: {
+            [Symbol.iterator]() {
+              let x = 0;
+              return {
+                next() {
+                  return {
+                    value: $this.getItem(x, y - 1),
+                    done: x++ >= $this.widthCount,
+                  };
+                },
+              };
             },
+          },
+          done: y++ >= $this.heightCount,
         };
-    }
+      },
+    };
+  }
 
-    getMapItemCount() {
-        this.widthCount = Math.ceil(this.width / this.boxSize);
-        this.heightCount = Math.ceil(this.height / this.boxSize);
-        return this.widthCount * this.heightCount;
-    }
+  getMapItemCount() {
+    this.widthCount = Math.ceil(this.width / this.boxSize);
+    this.heightCount = Math.ceil(this.height / this.boxSize);
+    return this.widthCount * this.heightCount;
+  }
 }
--- src/objects/Vector2.ts
+++ src/objects/Vector2.ts
@@ -1,29 +1,29 @@
 export default class Vector2 {
-    x = 0;
-    y = 0;
+  x = 0;
+  y = 0;
 
-    /**
-     * @param {Number} x
-     * @param {Number} y
-     */
-    constructor(x = 0, y = 0) {
-        this.x = x;
-        this.y = y;
-    }
+  /**
+   * @param {Number} x
+   * @param {Number} y
+   */
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
 
-    /**
-     * @param {Vector2} vector
-     */
-    plus(vector: Vector2) {
-        if (vector instanceof Vector2) {
-            this.x += vector.x;
-            this.y += vector.y;
-        } else {
-            require('vector must be  instance of Vector2');
-        }
+  /**
+   * @param {Vector2} vector
+   */
+  plus(vector: Vector2) {
+    if (vector instanceof Vector2) {
+      this.x += vector.x;
+      this.y += vector.y;
+    } else {
+      require('vector must be  instance of Vector2');
     }
+  }
 }
 
 function require(msg: string) {
-    throw new Error('[Vector2]: ' + msg);
+  throw new Error('[Vector2]: ' + msg);
 }
